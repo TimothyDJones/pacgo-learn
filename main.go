ca64f5338887ea4360f574a3a61b61055c8ea87e
Tim Jones <tdjones74021@yahoo.com>
2019-10-30 17:10:26 -0500

    Add functions to set terminal mode and handle keyboard inputing, including ending game on 'ESC'.

--- main.go
+++ main.go
@@ -5,12 +5,14 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"os/exec"
 )
 
 var maze []string
 
 func main() {
 	// initialize game
+	defer cleanup()
 
 	// load resources
 	err := loadMaze()
@@ -25,6 +27,16 @@ func main() {
 		printScreen()
 
 		// process input
+		input, err := readInput()
+		if err != nil {
+			log.Printf("Error reading keyboard input: %v", err)
+			break
+		}
+		
+		// Exit game if user presses "ESC"
+		if input == "ESC" {
+			break
+		}
 
 		// process movement
 
@@ -58,8 +70,52 @@ func loadMaze() error {
 }
 
 func printScreen() {
+	clearScreen()
 	for _, line := range maze {
 		fmt.Println(line)
 	}
 }
 
+func init() {
+	cbTerm := exec.Command("stty", "cbreak", "-echo")
+	cbTerm.Stdin = os.Stdin
+
+	err := cbTerm.Run()
+	if err != nil { 
+		log.Fatalf("Unable to activate cbreak terminal mode: %v\n", err)
+	}
+}
+
+func cleanup() {
+	cookedTerm := exec.Command("stty", "-cbreak", "echo")
+	cookedTerm.Stdin = os.Stdin
+
+	err := cookedTerm.Run()
+	if err != nil {
+		log.Fatalf("Unable to activate cooked terminal mode: %v\n", err)
+	}
+}
+
+func readInput() (string, error) {
+	buffer := make([]byte, 100)
+
+	cnt, err := os.Stdin.Read(buffer)
+	if err != nil {
+		return "", err
+	}
+
+	if cnt == 1 && buffer[0] == 0x1b {  // If user pressed "ESC"...
+		return "ESC", nil
+	}
+
+	return "", nil
+}
+
+func clearScreen() {
+	fmt.Printf("\x1b[2J")
+	moveCursor(0, 0)
+}
+
+func moveCursor(row, col int) {
+	fmt.Printf("\x1b[%d;%df", row+1, col+1)
+}
