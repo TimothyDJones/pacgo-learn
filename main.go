a966e08bcab0c45b2b58cff9616f57e82352f0a4
Tim Jones <tdjones74021@yahoo.com>
2019-10-30 16:39:56 -0500

    Add functions to load maze from file and display maze on screen.

--- main.go
+++ main.go
@@ -1,17 +1,28 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"log"
+	"os"
 )
+
+var maze []string
 
 func main() {
 	// initialize game
 
 	// load resources
+	err := loadMaze()
+	if err != nil {
+		log.Printf("Error loading maze data: %v\n", err)
+		return
+	}
 
 	// game loop
 	for {
 		// update screen
+		printScreen()
 
 		// process input
 
@@ -27,6 +38,28 @@ func main() {
 		break
 
 		// repeat
+	}
+}
+
+func loadMaze() error {
+	f, err := os.Open("maze01.txt")
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+
+	scanner := bufio.NewScanner(f)
+	for scanner.Scan() {
+		line := scanner.Text()
+		maze = append(maze, line)
+	}
+
+	return nil
+}
+
+func printScreen() {
+	for _, line := range maze {
+		fmt.Println(line)
 	}
 }
 
